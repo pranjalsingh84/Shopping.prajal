0c6604d1b02d4e9672d64cb6e8073982c6b5640e
pranjalsingh84 <topranjal.singh@gmail.com>
2026-01-21 00:04:46 +0530

    Final CORS fix

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'https://shopping-prajal-backend.onrender.com';
+export const BASE_URL =
+  process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
